74419db088ea73323a5b7fc301c4b3ead1bea691
ruanndias <ruan_ndias@hotmail.com>
2020-12-23 20:14:04 -0300

    sem corrupção de código -> entidades encapsuladas

--- SmartGym.Domain/Entities/PersonalTrainer.cs
+++ SmartGym.Domain/Entities/PersonalTrainer.cs
@@ -1,5 +1,6 @@
 ﻿using SmartGym.Domain.Enums;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SmartGym.Domain.Entities
 {
@@ -7,18 +8,25 @@ namespace SmartGym.Domain.Entities
     {
         protected PersonalTrainer() { }
 
+        private IList<Student> _students;
+
         public PersonalTrainer(string name, string address)
         {
             Name = name;
             Address = address;
             Status = EStatusPersonalTrainer.Active;
-            Students = new List<Student>();
+            _students = new List<Student>();
         }
 
         public int Id { get; private set; }
         public string Name { get; private set; }
         public string Address { get; private set; }
         public EStatusPersonalTrainer Status { get; private set; }
-        public List<Student> Students { get; private set; }
+        public IReadOnlyCollection<Student> Students { get { return _students.ToArray();  } }
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
     }
 }
--- SmartGym.Domain/Entities/TrainingCenter.cs
+++ SmartGym.Domain/Entities/TrainingCenter.cs
@@ -7,21 +7,28 @@ namespace SmartGym.Domain.Entities
     {
         protected TrainingCenter() { }
 
+        private IList<Student> _students;
+
+        private IList<PersonalTrainer> _personalTrainers;
+
         public TrainingCenter(string name, string address)
         {
             Name = name;
             Address = address;
-            Students = new List<Student>();
-            PersonalTrainers = new List<PersonalTrainer>();
-
+            _students = new List<Student>();
+            _personalTrainers = new List<PersonalTrainer>();
         }
 
         public long Id { get; private set; }
         public string Name { get; private set; }
         public string  Address { get; private set; }
-        public List<Student> Students { get; private set; }
-        public List<PersonalTrainer> PersonalTrainers { get; private set; }
+        public IReadOnlyCollection<Student> Students { get; private set; }
+        public IReadOnlyCollection<PersonalTrainer> PersonalTrainers { get; private set; }
 
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
 
     }
 }
