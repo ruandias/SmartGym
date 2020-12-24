005da94089b9ddbe6a2aa1e77554a51a2ad63488
@t2m.rndias <t2m.rndias@terceiros.mag.com.br>
2020-12-24 12:31:30 -0300

    Revert "Alterado as entidades para a migrations funcionar no momento de teste!"
    This reverts commit 52e8d1a6b156257318ee8f31540f8d2eb42e7077.

--- SmartGym.Domain/Entities/Student.cs
+++ SmartGym.Domain/Entities/Student.cs
@@ -9,21 +9,16 @@ namespace SmartGym.Domain.Entities
     {
         protected Student() { }
 
-        public Student(string name, Address address)
+        public Student(Name name, Address address)
         {
             Name = name;
             Address = address;
             Status = EStatusStudent.Active;
         }
 
-        public int Id { get; private set; }
-        public string Name { get; private set; }
+        public long Id { get; private set; }
+        public Name Name { get; private set; }
         public Address Address { get; private set; }
         public EStatusStudent Status { get; private set; }
-        public int IdTrainingCenter { get; private set; }
-        public TrainingCenter TrainingCenter { get; private set; }
-        public int IdPersonalTrainer { get; private set; }
-        public PersonalTrainer PersonalTrainer { get; private set; }
-
     }
 }
