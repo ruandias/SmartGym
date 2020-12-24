2b93401c3f4d5e9877a357d9897a7fedca251926
ruandias <ruan_ndias@hotmail.com>
2020-12-24 20:30:37 -0300

    adicionado NotificationDependency

--- SmartGym.API/Startup.cs
+++ SmartGym.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SmartGym.Infra.CrossCutting.Filter;
 using SmartGym.Infra.CrossCutting.InversionOfControl;
 
 namespace SmartGym.API
@@ -18,10 +19,17 @@ namespace SmartGym.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(config =>
+            {
+                config.EnableEndpointRouting = false;
+                config.Filters.Add<NotificationFilter>();
+            });
 
             services.AddControllers();
             services.AddSqlDependency(Configuration);
             services.AddSwaggerDependency();
+            services.AddNotificationDependency();
+
 
         }
 
---
+++ SmartGym.Infra.CrossCutting/InversionOfControl/NotificationDependency.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SmartGym.Infra.Shared.Contexts;
+
+namespace SmartGym.Infra.CrossCutting.InversionOfControl
+{
+    public static class NotificationDependency
+    {
+        public static void AddNotificationDependency(this IServiceCollection services)
+        {
+            services.AddScoped<NotificationContext>();
+        }
+    }
+}
