02d4799f915ebcc7f02f700d334e759cca60b4bb
ruanndias <ruan_ndias@hotmail.com>
2020-12-23 22:15:30 -0300

    add projeto Infra.CrossCutting/InversaoDeControle

--- SmartGym.API/Startup.cs
+++ SmartGym.API/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SmartGym.Infra.CrossCutting.InversionOfControl;
 
 namespace SmartGym.API
 {
@@ -23,26 +16,20 @@ namespace SmartGym.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmartGym.API", Version = "v1" });
-            });
+            services.AddSwaggerDependency();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartGym.API v1"));
-            }
+
+            app.UseSwaggerDependency();
 
             app.UseHttpsRedirection();
 
---
+++ SmartGym.Infra.CrossCutting/InversionOfControl/SwaggerDependency.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace SmartGym.Infra.CrossCutting.InversionOfControl
+{
+    public static class SwaggerDependency
+    {
+        public static void AddSwaggerDependency(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "SmartGym API",
+                        Version = "v1",
+                        Description = "API REST created on ASP.NET Core 5,0",
+                    });
+            });
+        }
+
+        public static void UseSwaggerDependency(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartGym API");
+                c.DocumentTitle = "SmartGym API";
+                c.DocExpansion(DocExpansion.List);
+            });
+        }
+    }
+}
