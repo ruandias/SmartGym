9c63f92d1ba0764acb8db0a54fbb4ae80a11bc0d
ruandias <ruan_ndias@hotmail.com>
2020-12-25 02:33:01 -0300

    adicionado student controller

--- SmartGym.API/Controllers/StudentController.cs
+++ SmartGym.API/Controllers/StudentController.cs
@@ -1,41 +1,91 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SmartGym.Domain.Entities;
+using SmartGym.Domain.Interfaces;
+using SmartGym.Domain.Models;
+using System;
 
 namespace SmartGym.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class StudentController : ControllerBase
+    [Route("api/students")]
+    public class StudentController : Controller
     {
+        private readonly IServiceStudent _serviceStudent;
 
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok();
-        }
+        public StudentController(IServiceStudent serviceStudent) =>
+            _serviceStudent = serviceStudent;
 
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            return Ok();
-        }
 
         [HttpPost]
-        public IActionResult Post([FromBody]Student student)
+        public IActionResult Register([FromBody] CreateStudentModel studentModel)
         {
-            return NoContent();
+            try
+            {
+                var student = _serviceStudent.Insert(studentModel);
+
+                return Created($"/api/students/{student?.Id}", student?.Id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(int id)
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStudentModel studentModel)
         {
-            return Ok();
+            try
+            {
+                var user = _serviceStudent.Update(id, studentModel);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public IActionResult Remove([FromRoute] int id)
         {
-            return Ok();
+            try
+            {
+                _serviceStudent.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult RecoverAll()
+        {
+            try
+            {
+                var users = _serviceStudent.RecoverAll();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Recover([FromRoute] int id)
+        {
+            try
+            {
+                var user = _serviceStudent.RecoverById(id);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
