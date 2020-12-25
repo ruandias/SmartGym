0bdb92838be641cbb852108af1f1a9498573c99a
ruandias <ruan_ndias@hotmail.com>
2020-12-24 22:08:43 -0300

    criado interfaces do estudante

---
+++ SmartGym.Domain/Interfaces/IRepositoryStudent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SmartGym.Domain.Entities;
+
+
+namespace SmartGym.Domain.Interfaces
+{
+    public interface IRepositoryStudent
+    {
+        void Save(Student obj);
+
+        void Remove(int id);
+
+        Student GetById(int id);
+
+        IList<Student> GetAll();
+    }
+}
---
+++ SmartGym.Domain/Interfaces/IServiceStudent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using SmartGym.Domain.Models;
+
+namespace SmartGym.Domain.Interfaces
+{
+    public interface IServiceStudent
+    {
+        StudentModel Insert(CreateStudentModel userModel);
+
+        StudentModel Update(int id, UpdateStudentModel studentModel);
+
+        void Delete(int id);
+
+        StudentModel RecoverById(int id);
+
+        IEnumerable<StudentModel> RecoverAll();
+    }
+}
